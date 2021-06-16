c717c58355c3ade134e47843dfad1772dce9987a
tiemthuan <85550430+tiemthuan@users.noreply.github.com>
2021-06-16 11:26:30 +0700

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookLibrary
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public int Price { get; set; }
+    }
+    public class ProductData
+    {
+        private List<Product> ProductList = new List<Product>();
+        public void ListAll()
+        {
+            if (ProductList.Count == 0)
+            {
+                Console.WriteLine("     >>> Danh sach trong <<<");
+                return;
+            }
+            foreach (Product sv in ProductList)
+            {
+                Console.WriteLine("ID: " + sv.ID + "Name: " + sv.Name + "Publisher: " + sv.Publisher + "Price: " + sv.Price);
+            }
+        }
+        public Boolean AddNew()
+        {
+            Product s = new Product();
+            Console.WriteLine("Input ID: ");
+            s.ID =  int.Parse(Console.ReadLine());
+            Console.WriteLine("Input name: ");
+            s.Name = Console.ReadLine();
+            Console.WriteLine("Input publisher: ");
+            s.Publisher = Console.ReadLine();
+            Console.WriteLine("Input price: ");
+            s.Price = int.Parse(Console.ReadLine());
+            Product p = Find(s.ID);
+            if (p == null)
+            {
+                ProductList.Add(s);
+                Console.WriteLine("Add success!");
+            }
+            else
+            {
+                Console.WriteLine("Add fail!");
+                return false;
+            } 
+            return true;
+        }
+        public void Search()
+        {
+            Product s = new Product();
+            Console.WriteLine("Input ID want to Find: ");
+            s.ID = int.Parse(Console.ReadLine());
+            Product p = Find(s.ID);
+            if(p == null)
+            {
+                Console.WriteLine("ID does not exist ");
+                return;
+            }
+            else
+            {
+                
+                Console.WriteLine("this is your book: ");
+                Console.WriteLine("ID: " + p.ID + "Name: " + p.Name + "Publisher: " + p.Publisher + "Price: " + p.Price);
+            }
+            return;
+        }
+        public Boolean Remove()
+        {
+            Console.WriteLine("Input ID: ");
+            int ID = int.Parse(Console.ReadLine());
+            Product p = Find(ID);
+            if(p != null)
+            {
+                ProductList.Remove(p);
+                Console.WriteLine("Remove success!");
+            }
+            else
+            {
+                Console.WriteLine("ID does not exist!");
+                return false;
+            }
+            return true;
+
+
+        }
+        public Product Find(int ID)
+        {
+            Product p = ProductList.SingleOrDefault(book => book.ID == ID);
+            return p;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            ArrayList menu = new ArrayList();
+            menu.Add("      ===== Manage books =====");
+            menu.Add("1. Add new book,");
+            menu.Add("2. Find a book,");
+            menu.Add("3. Show book,");
+            menu.Add("4. Remove a book,");
+            menu.Add("5. Quit");
+            MyBookLibrary.ProductData pro = new MyBookLibrary.ProductData();
+            do
+            {
+                do
+                {
+                    Console.WriteLine("===========================================");
+                    foreach (string show in menu)
+                    {
+                        Console.WriteLine(show);
+                    }
+                    Console.WriteLine("Choose from 1 to 5: ");
+                    choice = int.Parse(Console.ReadLine());
+                    if (choice < 1 || choice > 5) Console.WriteLine("input from 1 to 5");
+                } while (choice < 1 || choice > 5);
+                switch (choice)
+                {
+                    case 1:
+                        pro.AddNew();
+                        break;
+                    case 2:
+                        pro.Search();
+                        break;
+                    case 3:
+                        pro.ListAll();
+                        break;
+                    case 4:
+                        pro.Remove();
+                        break;
+
+                }
+                Console.WriteLine("===========================================\n");
+            } while (choice != 5);
+        }
+    }
+}
